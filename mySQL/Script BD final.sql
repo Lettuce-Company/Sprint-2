a3a23a40fb139f99162a8a19636a17e461856b41
Gisele-Silva325 <gisele.silva@sptech.school>
2023-04-23 15:37:24 -0300

    correcao script e modelagem

--- mySQL/Script BD final.sql
+++ mySQL/Script BD final.sql
@@ -3,58 +3,56 @@ create database lettuce;
 use lettuce;
 
 create table Empresa (
-idEmpresa int primary key auto_increment,
+-- idEmpresa int primary key auto_increment,
 nome varchar(45) not null,
-cnpj char(18) unique not null,
+cnpj char(14) primary key unique not null,
 telefone char(14)) auto_increment = 100;
 
 
 create table Funcionario(
-idFuncionario int auto_increment,
+-- idFuncionario int auto_increment,
 nome varchar(30) not null,
 sobrenome varchar(40) not null,
-cpf char(14) unique,
+cpf char(11) primary key unique,
 cargo varchar(30) not null,
 email varchar(45) unique not null,  constraint ckhEmailfunc check(Email like '%@%'),
 senha char(14) not null,
-dtNasc date not null,
-
-fkEmpresa int, constraint fkEmpresa foreign key(fkEmpresa) 
-references Empresa(idEmpresa),
-constraint pkFuncEmp primary key (idFuncionario, fkEmpresa), -- fk dependente
-
-fkChefe int, constraint FkChefe foreign key (fkChefe)
-references Funcionario(idFuncionario) -- fk self related
+-- dtNasc date not null,
+fkEmpresa char(14), constraint frkEmpresa foreign key(fkEmpresa) 
+references Empresa(cnpj),
+constraint pkFuncEmp primary key (cpf, fkEmpresa), -- fk dependente
+fkChefe char(11), constraint FrkChefe foreign key (fkChefe)
+references Funcionario(cpf) -- fk self related
 
 ) auto_increment = 1000;
 
 select * from Funcionario;
 
 create table Caminhao(
-idCaminhao int primary key auto_increment,
-placa char(7) unique not null,
-fkEmpresa int, constraint fkEmpTrans foreign key(fkEmpresa) references Empresa(idEmpresa) -- ex
+-- idCaminhao int primary key auto_increment,
+placa char(7) primary key unique not null,
+fkEmpresa char(14), constraint fkEmpTrans foreign key(fkEmpresa) references Empresa(cnpj) -- ex
 ) auto_increment = 10;
 
 create table Carga(
 idCarga int auto_increment,
-tipo varchar(45),
+produto varchar(45),
 temperaturaMin decimal(4,2),
 temperaturaMax decimal(4,2),
 UmidadeMin varchar (5),
 UmidadeMax varchar (5),
-fkCaminhao int, 
+fkCaminhao char(7), 
 constraint fkCaminhao foreign key(fkCaminhao) 
-references Caminhao(idCaminhao),
+references Caminhao(placa),
 constraint pkCargaCam primary key (idCarga, fkCaminhao) -- fk dependente de caminhao
 );
 
 create table Sensor(
 idSensor int primary key not null auto_increment,
 tipo varchar(5) not null, constraint chkSensor check(tipo IN("DHT11", "LM35")),
-fkCaminhao INT,
+fkCaminhao char(7),
  constraint FkSensorCaminhao foreign key (fkCaminhao)
- references Caminhao(idCaminhao));
+ references Caminhao(placa));
 
 create table Leitura (
 idLeitura int auto_increment,
