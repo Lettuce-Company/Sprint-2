7a99c9db9d15f2ee115ce156fa4b2b5e15f32178
Gisele-Silva325 <gisele.silva@sptech.school>
2023-05-29 11:46:15 -0300

    att login e cadastro com sha2

--- webdatalettuce/webdataviz/site/src/models/usuarioModel.js
+++ webdatalettuce/webdataviz/site/src/models/usuarioModel.js
@@ -12,19 +12,19 @@ function listar() {
 function entrar(email, senha) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", email, senha)
     var instrucao = `
-        SELECT * FROM Funcionario WHERE email = '${email}' AND senha = '${senha}';
+        SELECT * FROM Funcionario WHERE email = '${email}' AND senha = sha2('${senha}',256);
     `;
     console.log("Executando a instrução SQL: \n" + instrucao);
     return database.executar(instrucao);
 }
 
 // Coloque os mesmos parâmetros aqui. Vá para a var instrucao
-function cadastrar(nome, sobrenome, cpf, cargo, email, senha, nomeEmpr, cnpj, telefone) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, cpf, cargo, email, senha, nomeEmpr, cnpj, telefone);
+function cadastrar(nome, sobrenome, cpf, cargo, email, senha, nomeEmpr, cnpj, telefone, telefoneFunc, dtNasc) {
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, sobrenome, cpf, cargo, email, senha, nomeEmpr, cnpj, telefone, telefoneFunc, dtNasc);
     // Insira exatamente a query do banco aqui, lembrando da nomenclatura exata nos valores
     //  e na ordem de inserção dos dados.
     var instrucao = `INSERT INTO Empresa (nomeEmpr, cnpj, telefone) VALUES ('${nomeEmpr}','${cnpj}', '${telefone}');`;
-    var instrucao2 = `INSERT INTO Funcionario (nome, sobrenome, adm, cpf, cargo, email, senha, fkEmpresa) VALUES ('${nome}', '${sobrenome}', true, '${cpf}', '${cargo}', '${email}', '${senha}', '${cnpj}');`;
+    var instrucao2 = `INSERT INTO Funcionario (nome, sobrenome, adm, cpf, dtNasc, cargo, telefone, email, senha, fkEmpresa) VALUES ('${nome}', '${sobrenome}', true, '${cpf}', '${dtNasc}', '${cargo}', '${telefoneFunc}', '${email}', sha2('${senha}',256), '${cnpj}');`;
 
     console.log("Executando a instrução SQL: \n" + instrucao + instrucao2);
     database.executar(instrucao);
