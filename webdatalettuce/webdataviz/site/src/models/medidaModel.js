6379d7b4868ef6cc3af4852fa1579684f8d677d4
Kayky <kayky.pedroso@sptech.school>
2023-06-01 20:17:20 -0300

    add(): KPIS atuais funcionando, KPIS máx e min em progesso

--- webdatalettuce/webdataviz/site/src/models/medidaModel.js
+++ webdatalettuce/webdataviz/site/src/models/medidaModel.js
@@ -1,6 +1,6 @@
 var database = require("../database/config");
 
-function buscarUltimasMedidas(placa, limite_linhas) {
+function buscarUltimasMedidas(placa) {
 
     instrucaoSql = ''
 
@@ -14,13 +14,10 @@ function buscarUltimasMedidas(placa, limite_linhas) {
                     where fk_aquario = ${placa}
                     order by id desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
-        temperatura, 
-        umidade,
-                        dtHora,
-                    from leitura
-                    where fk_aquario = ${placa}
-                    order by id desc limit ${limite_linhas}`;
+        instrucaoSql = `SELECT temperatura, umidade, dtHora FROM VW_KPI_Atuais WHERE fkCaminhao = '${placa}' AND fkSensor  = (SELECT 
+            idSensor
+            FROM Sensor WHERE fkCaminhaoSensor = '${placa}')
+            ORDER BY idLeitura DESC LIMIT 1`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -40,17 +37,41 @@ function buscarMedidasEmTempoReal(placa) {
         dht11_umidade as umidade,  
                         CONVERT(varchar, momento, 108) as momento_grafico, 
                         fk_aquario 
-                        from medida where fk_aquario = ${placa} 
+                        from medida where fk_aquario = '${placa}' 
                     order by id desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
-        instrucaoSql = `select 
+        instrucaoSql = `SELECT temperatura, umidade, dtHora FROM VW_KPI_Atuais WHERE fkCaminhao = '${placa}' AND fkSensor  = (SELECT 
+            idSensor
+            FROM Sensor WHERE fkCaminhaoSensor = '${placa}')
+            ORDER BY idLeitura DESC LIMIT 7`;
+    } else {
+        console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
+        return
+    }
+
+    console.log("Executando a instrução SQL: \n" + instrucaoSql);
+    return database.executar(instrucaoSql);
+}
+
+function buscarMedidasMaximasMinimas(placa) {
+
+    instrucaoSql = ''
+
+    if (process.env.AMBIENTE_PROCESSO == "producao") {
+        instrucaoSql = `select top 1
         dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,
-                        DATE_FORMAT(momento,'%H:%i:%s') as momento_grafico, 
+        dht11_umidade as umidade,  
+                        CONVERT(varchar, momento, 108) as momento_grafico, 
                         fk_aquario 
-                        from medida where fk_aquario = ${placa} 
-                    order by id desc limit 1`;
+                        from medida where fk_aquario = '${placa}' 
+                    order by id desc`;
+
+    } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
+        instrucaoSql = `SELECT temperatura, umidade, dtHora FROM VW_KPI_Atuais WHERE fkCaminhao = '${placa}' AND fkSensor  = (SELECT 
+            idSensor
+            FROM Sensor WHERE fkCaminhaoSensor = '${placa}')
+            ORDER BY idLeitura DESC LIMIT 7`;
     } else {
         console.log("\nO AMBIENTE (produção OU desenvolvimento) NÃO FOI DEFINIDO EM app.js\n");
         return
@@ -61,7 +82,9 @@ function buscarMedidasEmTempoReal(placa) {
 }
 
 
+
 module.exports = {
     buscarUltimasMedidas,
-    buscarMedidasEmTempoReal
+    buscarMedidasEmTempoReal,
+    buscarMedidasMaximasMinimas
 }
