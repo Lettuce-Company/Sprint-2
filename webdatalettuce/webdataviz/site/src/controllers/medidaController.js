900e9c0627177683ba1dbf0fb7c46cafda27f599
Kayky <kayky.pedroso@sptech.school>
2023-06-02 01:14:45 -0300

    Function(): KPIS completas

--- webdatalettuce/webdataviz/site/src/controllers/medidaController.js
+++ webdatalettuce/webdataviz/site/src/controllers/medidaController.js
@@ -41,28 +41,28 @@ function buscarMedidasEmTempoReal(req, res) {
     });
 }
 
-function buscarMedidasMaximasMinimas(req, res) {
+// function buscarMedidasMaximasMinimas(req, res) {
 
-    var placa = req.params.placa;
+//     var placa = req.params.placa;
 
-    console.log(`Recuperando medidas em tempo real`);
+//     console.log(`Recuperando medidas em tempo real`);
 
-    medidaModel.buscarMedidasMaximasMinimas(placa).then(function (resultado) {
-        if (resultado.length > 0) {
-            res.status(200).json(resultado);
-        } else {
-            res.status(204).send("Nenhum resultado encontrado!")
-        }
-    }).catch(function (erro) {
-        console.log(erro);
-        console.log("Houve um erro ao buscar as ultimas máximas e mínimas.", erro.sqlMessage);
-        res.status(500).json(erro.sqlMessage);
-    });
-}
+//     medidaModel.buscarMedidasMaximasMinimas(placa).then(function (resultado) {
+//         if (resultado.length > 0) {
+//             res.status(200).json(resultado);
+//         } else {
+//             res.status(204).send("Nenhum resultado encontrado!")
+//         }
+//     }).catch(function (erro) {
+//         console.log(erro);
+//         console.log("Houve um erro ao buscar as ultimas máximas e mínimas.", erro.sqlMessage);
+//         res.status(500).json(erro.sqlMessage);
+//     });
+// }
 
 module.exports = {
     buscarUltimasMedidas,
     buscarMedidasEmTempoReal,
-    buscarMedidasMaximasMinimas
+    // buscarMedidasMaximasMinimas
 
 }
