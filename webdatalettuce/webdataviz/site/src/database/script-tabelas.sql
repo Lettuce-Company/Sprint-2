015ddd050e8089ffa9eb4c0f43323cd08b591501
Kayky <kayky.pedroso@sptech.school>
2023-06-07 21:57:45 -0300

    Fix(): Script limpo

--- webdatalettuce/webdataviz/site/src/database/script-tabelas.sql
+++ webdatalettuce/webdataviz/site/src/database/script-tabelas.sql
@@ -1,4 +1,4 @@
-DROP DATABASE lettuce;
+
 CREATE DATABASE IF NOT EXISTS lettuce;
 use lettuce;
 
@@ -12,7 +12,6 @@ telefone char(14)
 insert into Empresa values
 ('JuicyLettuce', '12345678965478', '11939249423');
 
-select * from Empresa;
 
 create table if not exists Funcionario(
 idFuncionario int auto_increment,
@@ -35,11 +34,6 @@ references Funcionario(idFuncionario) -- fk self related
 
 ) auto_increment = 1000;
 
-INSERT INTO Funcionario VALUES
-(NULL, 'Marcos', 'Telúrio', '12345678900', 'Gerente', '11913829401', 'marcos.telurio@gmail.com', '1980-02-15', true, true, sha2('yub56m8123459', 256), '12345678965478', null),
-(NULL, 'Antônio', 'Figueredes', '29345678900', 'Funcionário', '11938291931', 'antonio@gmail.com', '1994-12-07', false, true, sha2('abcacsf28183', 256), '12345678965478', 1000);
-
-select * from Funcionario;
 
 create table if not exists Caminhao(
 -- idCaminhao int primary key auto_increment,
@@ -48,11 +42,7 @@ fkEmpresaVeiculo char(14), constraint fkEmpTrans foreign key(fkEmpresaVeiculo) r
 );
 
 insert into Caminhao values
-('GDU2589', '12345678965478'),
-('CTU1350', '12345678965478');
-
-select * from Caminhao;
-
+('GDU2589', '12345678965478');
 
 create table if not exists Carga(
 idCarga int primary key auto_increment,
@@ -61,11 +51,6 @@ qtdKg DECIMAL (7,2),
 dtHr datetime
 ) auto_increment = 100;
 
-INSERT INTO Carga VALUES
-(null, 'Alface Crespa', 1000, '2023-04-21 14:00:21'),
-(null, 'Rúcula', 450, '2023-04-13 16:18:55');
-
-SELECT * FROM Carga;
 
 create table if not exists Motorista(
 idMotorista INT PRIMARY KEY auto_increment,
@@ -74,12 +59,6 @@ email VARCHAR(45),
 telefone CHAR(11),
 CNH CHAR(11)
 );
-
-INSERT INTO Motorista VALUES
-(null, 'Henrique', 'henrique@outlook.com','11948294812', '76441275757'),
-(null, 'Victor', 'victor@outlook.com', '11956278430', '06295081247');
-
-SELECT * FROM Motorista;
 
 create table if not exists Viagem(
 idViagem INT auto_increment,
@@ -114,8 +93,7 @@ fkCaminhaoSensor char(7),
  references Caminhao(placa));
  
 insert into sensor values
-(null, 'DHT11', 'GDU2589'),
-(null, 'LM35', 'CTU1350');
+(null, 'DHT11', 'GDU2589');
 
 SELECT * FROM sensor;
 
@@ -147,7 +125,6 @@ references Sensor(idSensor),
 (null, 6, 70.23, default, 2),
 (null, 10, 0.23, default, 2);
 
-SELECT * FROM Leitura;
 
 CREATE VIEW VW_Caminhao_leitura AS
 select 
@@ -184,10 +161,6 @@ ON fkEmpresaVeiculo = cnpj
 JOIN Viagem
 ON fkCarga = idCarga;
 
--- View da KPI dos extremos (temperaturas máximas e mínimas do caminhão)
-SELECT max(temperatura), max(umidade), min(temperatura), min(umidade), dtHora FROM VW_KPI_Extremos WHERE fkCaminhao = 'CTU1350' AND fkSensor  = 2
-GROUP BY idLeitura,  dtHora, fkcaminhao
-ORDER BY idLeitura DESC LIMIT 7;
 
  CREATE VIEW VW_KPI_Atuais AS
 SELECT 
@@ -209,16 +182,6 @@ ON fkEmpresaVeiculo = cnpj
 JOIN Viagem
 ON fkCarga = idCarga;
 
--- View da KPI dos extremos (Ultima temperatura e umidade do caminhão)
-SELECT temperatura, umidade, dtHora FROM VW_KPI_Atuais WHERE fkCaminhao = 'CTU1350' AND fkSensor  = (SELECT 
-idSensor
-FROM Sensor WHERE fkCaminhaoSensor = 'CTU1350')
-ORDER BY idLeitura DESC LIMIT 1;
-
-
-SELECT 
-idSensor
-FROM Sensor WHERE fkCaminhaoSensor = 'CTU1350';
 
 CREATE VIEW VW_Motorista AS
 SELECT 
@@ -228,13 +191,6 @@ Motorista.telefone,
 Motorista.CNPJ 
 FROM Motorista JOIN Viagem 
 ON idMotorista = fkMotorista;
-
--- View dos dados do motorista
-SELECT * FROM VW_Motorista;
-
-SELECT 
-idSensor
-FROM Sensor WHERE fkCaminhaoSensor = 'CTU1350';
 
 
 CREATE VIEW VW_Func AS
@@ -251,8 +207,6 @@ ON Funcionario.fkChefe = Chefe.idFuncionario
 JOIN Empresa
 ON cnpj = Funcionario.fkEmpresa;
 
--- View dos Dados dos Funcionários
-SELECT * FROM VW_Func;
 
 CREATE VIEW VW_Carga AS 
 SELECT 
@@ -266,54 +220,4 @@ FROM Carga
 JOIN Viagem ON 
 idCarga = Viagem.fkCarga;
 
--- View dos dados da carga
-SELECT * FROM VW_Carga;
 
-
-
-
--- Procedimento, semelhante a uma função, Realiza as ações contidas nela.
-DELIMITER $$
-CREATE PROCEDURE Select_All()
-BEGIN
-SELECT * FROM Empresa;
-SELECT * FROM Funcionario;
-SELECT * FROM Caminhao;
-SELECT * FROM Carga;
-SELECT * FROM Motorista;
-SELECT * FROM Viagem;
-SELECT * FROM Sensor;
-SELECT * FROM Leitura;
-SELECT * FROM VW_Caminhao_Leitura;
-SELECT * FROM VW_Func;
-END$$
-DELIMITER ;
-
--- Se quiser dar um select em tudo até agora, use a PROCEDURE abaixo:
-CALL Select_All()
-
--- Operações matemáticas
--- Aviso!! As operações matemáticas foram comentadas para manter o SELECT'S simplistas, facilitando a visua-
--- lização dos dados presentes nas tabelas a partir do botão execute visto como um raio no topo da tela. 
-
--- select max(umidade) as 'umidade mais alta' from leitura; -- select temperatura mais alta 
-
--- select min(umidade) as 'umidade mais baixa' from leitura; -- select temperatura mais baixa 
-
--- select max(temperatura) as 'temp mais alta', min(temperatura) as 'temp mais baixa' from leitura;
-
--- select round (avg(temperatura), 2) as `media de temperatura` from leitura; -- select média (avg) arendando (round) com 2 casas após a virgula round(dado, casas apos vírgula)
-
-
--- SELECT * FROM leitura WHERE temperatura > (SELECT AVG(temperatura) FROM leitura); -- select onde a temperatura está acima da média
-
--- select upper(empresa.nomeEmpr) from empresa; -- selecionar tudo em maiusculo
-
--- select UCASE(empresa.nomeEmpr) from empresa; -- selecionar tudo em maiusculo, n sei a diferença entre eles
-
--- select lower(empresa.nomeEmpr) from empresa; -- selecionar tudo em minusculo
-
--- select leitura.temperatura, leitura.dtHora, sensor.tipo from leitura join sensor on fkSensor = idSensor where tipo = 'LM35';
-
-
- 
